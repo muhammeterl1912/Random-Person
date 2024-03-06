a4e256e344c91b44ede2b2f7efc02c5cd3d6fdaa
Muhammet Erol <muhammederol971@gmail.com>
2024-03-06 14:35:46 +0100

    finished the proejct.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+// Selectors
+const img = document.querySelector(".random-picture");
+const randomTitle = document.querySelector(".random-title");
+const randomName = document.querySelector(".random-first");
+const randomLastName = document.querySelector(".random-last");
+const mail = document.querySelector(".e-mail span");
+const adress = document.querySelector(".adress span");
+const phone = document.querySelector(".phone span");
+const button = document.querySelector("button");
 
 fetch("https://randomuser.me/api/")
   .then((response) => response.json())
   .then((data) => {
     let result = data.results[0];
- 
-    console.log(result)
+    console.log(result);
 
+    img.src = result.picture.large;
+    randomTitle.textContent += `${result.name.title}.`;
+    randomName.textContent += result.name.first;
+    randomLastName.textContent += result.name.last;
+    mail.textContent += result.email;
+    adress.textContent += `${result.location.street.name} ${result.location.city} ${result.location.postcode} ${result.location.country}`;
+    phone.textContent += result.phone;
   })
   .catch((error) => {
     console.error("Hata:", error);
   });
+button.addEventListener("click",()=>{location.reload();
+})
