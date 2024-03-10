b550bb0dae5f7ee81af20c6adb235f034426496c
Muhammet Erol <muhammederol971@gmail.com>
2024-03-10 18:58:17 +0100

    developed the code.

--- app.js
+++ app.js
@@ -7,30 +7,44 @@ const mail = document.querySelector(".e-mail span");
 const adress = document.querySelector(".adress span");
 const phone = document.querySelector(".phone span");
 const button = document.querySelector("button");
+const body = document.querySelector("body");
 
 // Fetch
-fetch("https://randomuser.me/api/")
-  .then((response) => {
-if(!response.ok){
-  throw new Error("Something went wrong!")
-}
-   return  response.json()
-  })
-  .then((data) => {
-    let result = data.results[0];
-    console.log(result);
+const fetchData = async () => {
+  try {
+    const response = await fetch("https://randomuser.me/api/a");
+    console.log(response);
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
 
-    img.src = result.picture.large;
-    randomTitle.textContent += `${result.name.title}.`;
-    randomName.textContent += result.name.first;
-    randomLastName.textContent += result.name.last;
-    mail.textContent += result.email;
-    adress.textContent += `${result.location.street.name} ${result.location.city} ${result.location.postcode} ${result.location.country}`;
-    phone.textContent += result.phone;
-  })
-  .catch((error) => {
-  document.write()
-  });
+    const data = await response.json();
+    responseData(data);
+  } catch (error) {
+    errorHandling(error);
+  }
+};
+
+fetchData();
+
+const responseData = (data) => {
+  let result = data.results[0];
+
+  img.src = result.picture.large;
+  randomTitle.textContent += `${result.name.title}.`;
+  randomName.textContent += result.name.first;
+  randomLastName.textContent += result.name.last;
+  mail.textContent += result.email;
+  adress.textContent += `${result.location.street.name} ${result.location.city} ${result.location.postcode} ${result.location.country}`;
+  phone.textContent += result.phone;
+};
+
+// Error Handling
+const errorHandling = (error) => {
+  body.innerHTML = `<h3>${error}</h3>`;
+};
+
 //   Event
-button.addEventListener("click",()=>{location.reload();
-})
+button.addEventListener("click", () => {
+  location.reload();
+});
